689c11efb1c6579c5c4266a82e88fb4c3133d5fe
Eric <erjohnson.pdx@gmail.com>
2014-08-30 18:39:36 -0700

    Define Users and Ideas db tables

--- db/migrate/20140831013533_create_ideas.rb
+++ db/migrate/20140831013533_create_ideas.rb
@@ -1,7 +1,10 @@
 class CreateIdeas < ActiveRecord::Migration
   def change
     create_table :ideas do |t|
-
+      t.string :name
+      t.string :description
+      t.integer :votes
+      t.belongs_to :user
       t.timestamps
     end
   end
